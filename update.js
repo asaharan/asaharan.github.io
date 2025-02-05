03831cc8f539c4f5434ce7963fb761f9392492a1
Amit Saharan <asaharan812@gmail.com>
2025-02-05 12:08:48 +0530

    add tree visualiser

---
+++ update.js
@@ -0,0 +1,37 @@
+fetch(
+  "https://smartbuy.myimaginestore.com/customer/address/formPost/id/103241/",
+  {
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "accept-language": "en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6",
+      "cache-control": "no-cache",
+      "content-type":
+        "multipart/form-data; boundary=----WebKitFormBoundarynbVFCzecBgIgNSoz",
+      "device-memory": "8",
+      downlink: "1.45",
+      dpr: "1.25",
+      ect: "3g",
+      pragma: "no-cache",
+      priority: "u=0, i",
+      rtt: "600",
+      "sec-ch-ua":
+        '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+      "viewport-width": "1152",
+      cookie:
+        "cf_clearance=kkydyL6p4U1YPnM8fy7YHQCjwc6CELVd3L3XjrdleTU-1704393727-0-2-433df5b6.394fe447.1dbb5655-0.2.1704393727; searchReport-log=0; form_key=WkMxnmBBkc7apJeS; mage-cache-storage=%7B%7D; mage-cache-storage-section-invalidation=%7B%7D; mage-cache-sessid=true; recently_viewed_product=%7B%7D; recently_viewed_product_previous=%7B%7D; recently_compared_product=%7B%7D; recently_compared_product_previous=%7B%7D; product_data_storage=%7B%7D; twk_idm_key=Jk25HtBH0C1BP1yeK-CnR; PHPSESSID=nk6goa5pjeq9m7k4fr5j3dbu4c; form_key=WkMxnmBBkc7apJeS; X-Magento-Vary=9bf9a599123e6402b85cde67144717a08b817412; mage-messages=; private_content_version=57a7962c5a2611ebfe6b1d6d32e654bd; TawkConnectionTime=0; section_data_ids=%7B%22cart%22%3A1730647027%2C%22customer%22%3A1730647027%2C%22compare-products%22%3A1730647027%2C%22last-ordered-items%22%3A1730647027%2C%22directory-data%22%3A1730647027%2C%22captcha%22%3A1730647027%2C%22instant-purchase%22%3A1730647027%2C%22loggedAsCustomer%22%3A1730647027%2C%22persistent%22%3A1730647027%2C%22review%22%3A1730647027%2C%22wishlist%22%3A1730647027%2C%22bss-fbpixel-atc%22%3A1730647027%2C%22bss-fbpixel-subscribe%22%3A1730647027%2C%22chatData%22%3A1730647027%2C%22recently_viewed_product%22%3A1730647027%2C%22recently_compared_product%22%3A1730647027%2C%22product_data_storage%22%3A1730647027%2C%22paypal-billing-agreement%22%3A1730647027%7D",
+      Referer:
+        "https://smartbuy.myimaginestore.com/customer/address/edit/id/103241/",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+    },
+    body: '------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="form_key"\r\n\r\nWkMxnmBBkc7apJeS\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="success_url"\r\n\r\n\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="error_url"\r\n\r\n\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="firstname"\r\n\r\nAnita\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="lastname"\r\n\r\nGaat\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="company"\r\n\r\n\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="telephone"\r\n\r\n+919996767445\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="street[]"\r\n\r\nRoom 25, PG 1 Hostel, MBS Hospital Nayapura\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="vat_id"\r\n\r\n\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="country_id"\r\n\r\nIN\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="region_id"\r\n\r\n513\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="region"\r\n\r\nRajasthan\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="city"\r\n\r\nKota\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz\r\nContent-Disposition: form-data; name="postcode"\r\n\r\n324001\r\n------WebKitFormBoundarynbVFCzecBgIgNSoz--\r\n',
+    method: "POST",
+  }
+);
