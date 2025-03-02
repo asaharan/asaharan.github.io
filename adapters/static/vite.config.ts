da0cb70e604aa21551695c59bf052adff42421d0
Amit Saharan <asaharan812@gmail.com>
2025-03-02 10:40:16 +0530

    change canonical url to saharan.me

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
 		},
 		plugins: [
 			staticAdapter({
-				origin: "https://amit.saharan.me",
+				origin: "https://saharan.me",
 			}),
 		],
 	};
